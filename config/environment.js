0be41cd958732c34216ad209e969a9ebdbf889a5
Kex1016 <nyilasdavid@gmail.com>
2019-05-09 18:32:06 +0200

    Revert "added redirect to my site"
    This reverts commit 5f57801984dc06933a36fef78733d2b3b19b466b.

--- config/environment.js
+++ config/environment.js
@@ -1,77 +1,73 @@
 'use strict';
 
-module.exports = function (environment) {
-	let ENV = {
-		modulePrefix: 'empress-blog-casper',
-		environment,
-		rootURL: '/',
-		locationType: 'auto',
+module.exports = function(environment) {
+  let ENV = {
+    modulePrefix: 'empress-blog-casper',
+    environment,
+    rootURL: '/',
+    locationType: 'auto',
 
-		EmberENV: {
-			FEATURES: {
-				// Here you can enable experimental features on an ember canary build
-				// e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
-			},
-			EXTEND_PROTOTYPES: {
-				// Prevent Ember Data from overriding Date.parse.
-				Date: false
-			}
-		},
-
-		APP: {
-			// Here you can pass flags/options to your application instance
-			// when it is created
-		},
-
-		blog: {
-			title: "Kex's blog",
-			description: 'Full of weeb since 2019.',
-			coverImage: '/images/bcover.png',
-
-			navigation: [{
-					label: 'Home',
-					route: 'index'
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
       },
-				{
-					label: 'Website',
-					route: 'https://kex1016.ga'
-		}]
-		},
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
+      }
+    },
 
-		'responsive-image': {
-			sourceDir: 'images',
-			destinationDir: 'responsive-images',
-			quality: 80,
-			supportedWidths: [2000, 1000, 600, 300],
-			removeSourceDir: false,
-			justCopy: false,
-			extensions: ['jpg', 'jpeg', 'png', 'gif']
-		}
-	};
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    },
 
-	if (environment === 'development') {
-		// ENV.APP.LOG_RESOLVER = true;
-		// ENV.APP.LOG_ACTIVE_GENERATION = true;
-		// ENV.APP.LOG_TRANSITIONS = true;
-		// ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-		// ENV.APP.LOG_VIEW_LOOKUPS = true;
-	}
+    blog: {
+      title: "Kex's blog",
+      description: 'Full of weeb since 2019.',
+      coverImage: '/images/bcover.png',
 
-	if (environment === 'test') {
-		// Testem prefers this...
-		ENV.locationType = 'none';
+      navigation: [{
+        label: 'Home',
+        route: 'index'
+      }]
+    },
 
-		// keep test console output quieter
-		ENV.APP.LOG_ACTIVE_GENERATION = false;
-		ENV.APP.LOG_VIEW_LOOKUPS = false;
+    'responsive-image': {
+      sourceDir: 'images',
+      destinationDir: 'responsive-images',
+      quality: 80,
+      supportedWidths: [2000, 1000, 600, 300],
+      removeSourceDir: false,
+      justCopy: false,
+      extensions: ['jpg', 'jpeg', 'png', 'gif']
+    }
+  };
 
-		ENV.APP.rootElement = '#ember-testing';
-		ENV.APP.autoboot = false;
-	}
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
 
-	if (environment === 'production') {
-		// here you can enable a production-specific feature
-	}
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.locationType = 'none';
 
-	return ENV;
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
+    ENV.APP.autoboot = false;
+  }
+
+  if (environment === 'production') {
+    // here you can enable a production-specific feature
+  }
+
+  return ENV;
 };
